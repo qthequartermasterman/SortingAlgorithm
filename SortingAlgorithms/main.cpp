06fbe1cdb47263c1c5ab17a6ad6ea4098564427a
aps0127 <aps0127@unt.edu>
2020-03-31 20:28:24 -0500

    Implmented main. Creates random array and sorts it.

--- SortingAlgorithms/main.cpp
+++ SortingAlgorithms/main.cpp
@@ -1,17 +1,31 @@
-///*=============main.cpp================*/
+/*=============main.cpp================*/
 /*
  DESCRIPTION:   
  AUTHOR:        Andrew P. Sansom
  VERSION:       1.0.0
  VERSION DATE:  3/31/20
  EMAIL:         andrewsansom@my.unt.edu
- COURSE:        CSCE 1040
  =============main.cpp================*/
 
 #include <iostream>
+#include "utilityFunctions.hpp"
+#include "MergeSort.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    const int size = 10;
+    
+    int array[size];
+    
+    //Initialize the array with random integers.
+    for (int i = 0; i < size; i++){
+        array[i] = rand();
+    }
+    //Print the unsorted array
+    std::cout << "Unsorted array.\n";
+    print_array(array, size);
+    
+    //Sort the array and print.
+    std::cout << "Sorted array.\n";
+    mergeSort(array, 0, size-1);
     return 0;
 }
