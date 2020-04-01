9f1b4b5324379ea6297d85e1c5bb98114cf4588b
aps0127 <aps0127@unt.edu>
2020-03-31 19:09:27 -0500

    Initial Commit

---
+++ SortingAlgorithms/main.cpp
@@ -0,0 +1,17 @@
+///*=============main.cpp================*/
+/*
+ DESCRIPTION:   
+ AUTHOR:        Andrew P. Sansom
+ VERSION:       1.0.0
+ VERSION DATE:  3/31/20
+ EMAIL:         andrewsansom@my.unt.edu
+ COURSE:        CSCE 1040
+ =============main.cpp================*/
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
